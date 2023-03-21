38bb9e7c125022f2a4906fc6801f165955781e86
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2023-03-21 21:17:55 +0900

    Fix test

--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -1,29 +1,29 @@
 import unittest
 
-from chromedriver_downloader import ChromeVersion
+from chromedriver_downloader import Chrome
 
-class TestChromeVersion(unittest.TestCase):
+class TestChrome(unittest.TestCase):
     
     def test_parse_with_google_chrome_version(self):
-        cv = ChromeVersion.parse("Google Chrome 111.0.5563.64\n\n")
+        cv = Chrome.parse("Google Chrome 111.0.5563.64\n\n")
         expect = "111.0.5563.64"
         actual = cv.version
         self.assertEqual(actual, expect)
 
     def test_parse_with_deb_version(self):
-        cv = ChromeVersion.parse("111.0.5563.64-1\n")
+        cv = Chrome.parse("111.0.5563.64-1\n")
         expect = "111.0.5563.64"
         actual = cv.version
         self.assertEqual(actual, expect)
         
     def test_parse(self):
-        cv = ChromeVersion.parse("111.0.5563.64\n")
+        cv = Chrome.parse("111.0.5563.64\n")
         expect = "111.0.5563.64"
         actual = cv.version
         self.assertEqual(actual, expect)
 
     def test_major_version(self):
-        cv = ChromeVersion.parse("111.0.5563.64\n")
+        cv = Chrome.parse("111.0.5563.64\n")
         expect = "111.0.5563"
         actual = cv.major_version
         self.assertEqual(actual, expect)
