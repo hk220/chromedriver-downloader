1c71c0d83c4b289379e0ec7e5dda88403be3057b
Kazuki Hara <kazuki.hara.dw3@gmail.com>
2023-03-21 12:39:45 +0900

    Add logging and catch exception.

--- chromedriver_downloader.py
+++ chromedriver_downloader.py
@@ -1,5 +1,9 @@
+import logging
 import subprocess
 import urllib.request
+import urllib.error
+
+logging.basicConfig(level=logging.DEBUG)
 
 LATEST_RELEASE_URL = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"
 CHROME_DRIVER_URL = "https://chromedriver.storage.googleapis.com/{version}/chromedriver_{arch}.zip"
@@ -26,16 +30,24 @@ def fetch_chromedriver_version(major_version: str) -> str:
     url = LATEST_RELEASE_URL + major_version
     req = urllib.request.Request(url)
     body = ""
-    with urllib.request.urlopen(req) as res:
-        body = res.read()
+    try:
+        with urllib.request.urlopen(req) as res:
+            body = res.read()
+    except urllib.error.URLError as e:
+        logging.critical(f"Fetch chromedriver version error: {e}")
+        exit(255)
     return body.decode("utf-8")
 
 def fetch_chromedriver(version: str) -> None:
     url = CHROME_DRIVER_URL.format(version=version, arch="linux64")
-    with urllib.request.urlopen(url) as res:
-        data = res.read()
-        with open("chromedriver.zip", "wb") as f:
-            f.write(data)
+    try:
+        with urllib.request.urlopen(url) as res:
+            data = res.read()
+            with open("chromedriver.zip", "wb") as f:
+                f.write(data)
+    except urllib.error.URLError as e:
+        logging.critical(f"Fetch chromedriver error: {e}")
+        exit(255)
 
 def get_chrome_version() -> str:
     output = subprocess.run(['google-chrome', '--version'], capture_output=True, text=True).stdout
@@ -43,9 +55,12 @@ def get_chrome_version() -> str:
 
 def main() -> None:
     chrome_version = get_chrome_version()
+    logging.info(f"google-chrome --version: {chrome_version.strip()}")
     version = parse_chrome_version(chrome_version)
+    logging.info(f"chrome version: {version}")
     major_version = get_major_version(version)
     chromedriver_version = fetch_chromedriver_version(major_version)
+    logging.info(f"chromedriver version: {chromedriver_version}")
     fetch_chromedriver(chromedriver_version)
     
 if __name__ == "__main__":
